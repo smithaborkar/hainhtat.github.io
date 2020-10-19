c70ae934e47b7a04defd08d0ad86092dda208d16
Hein Htet Aung <hainizinsane@gmail.com>
2020-10-19 22:44:09 +0630

    Updates

---
+++ precache-manifest.40a5bd15fd2b3b3f89ab691fa590b988.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0da548c573ac7c78aba7b57d672151e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ae14c1ca02ea6a571ed",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a2c8b4ceb71f7352926f",
+    "url": "/static/js/2.3b45fe8b.chunk.js"
+  },
+  {
+    "revision": "b13e737c23346402089bf79c557ecf58",
+    "url": "/static/js/2.3b45fe8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ae14c1ca02ea6a571ed",
+    "url": "/static/js/main.6d31ae1d.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "7d7a60541838716bb838d2f5b9f06fb1",
+    "url": "/static/media/profile.7d7a6054.jpg"
+  }
+]);
